53440867a80679b03a59ea01bcdd23a4f57a44c3
Şahin Dirim <sahindirim@Sahin-MacBook-Pro.local>
2014-09-12 18:00:54 +0300

    Initial Commit

---
+++ ssssss/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  ssssss
+//
+//  Created by Şahin Dirim on 12.09.2014.
+//  Copyright (c) 2014 Şahin Dirim. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
